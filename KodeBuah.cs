a32de29bbf2ad0eae06efcfcf2980ed7086ce7b5
unknown <nerdstorms27@gmail.com>
2024-03-16 08:52:59 +0700

    menambahkan kelas KodeBuah

---
+++ KodeBuah.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_1302220049
+{
+    internal class KodeBuah
+    {
+        public enum Buah { Apel, Aprikot, Alpukat, Pisang, Paprika, Blackberry, Ceri, Kelapa, Jagung, Kurma, Durian, Anggur, Melon, Semangka }
+
+        public static string getKodeBuah(Buah buah)
+        {
+            string[] kode = { "A00", "B00", "C00", "D00", "E00", "F00", "G00", "H00", "I00", "J00", "K00", "L00", "M00", "N00", "O00" };
+            {
+                return kode[(int)buah];
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using modul4_1302220049;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.Buah.Alpukat));
+    }
+}
